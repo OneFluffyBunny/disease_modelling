c0bdae6bb772db256677d10ae2dc2a63219f83f7
Andrei Raducea-Marin <60304950+OneFluffyBunny@users.noreply.github.com>
2020-05-01 22:31:41 +0100

    Added all the functions used in the 2 subpopulation model simulations

--- two_populations_SIR.py
+++ two_populations_SIR.py
@@ -74,6 +74,7 @@ def empirical_major_outbreak_probability_from_pickle(file_name, directory='heter
 
 
 def efficient_solver(a,b,c,d):
+    """Computes the outbreak probabilities in a very efficient manner"""
     if (1-2*a-b>0 and 1-2*c-d>0 and (1-2*a-b)*(1-2*c-d)>b*d):
         return [1,1]
     x_min = ((1 - np.sqrt(1 - 4 * a * (1 - a - b))) / (2 * a))
@@ -85,7 +86,7 @@ def efficient_solver(a,b,c,d):
 
 
 def homo_from_hetero(beta_11, beta_12, beta_21, beta_22, gamma_1, gamma_2):
-    """Reuturns the naive infection parameters one would get if assuming a homogeneous model"""
+    """Returns the naive infection parameters one would get if assuming a homogeneous model"""
     gamma = 2 * gamma_1 * gamma_2 / (gamma_1 + gamma_2)
     beta = ((beta_11 + beta_21) * gamma_2 + (beta_12 + beta_22) * gamma_1) / (gamma_1 + gamma_2)
     return (beta, gamma)
@@ -100,11 +101,10 @@ def homo_vs_hetero_probability(beta_11, beta_12, beta_21, beta_22, gamma_1, gamm
     rate_2 = beta_12 + beta_22 + gamma_2
     hetero_solutions = efficient_solver(a = beta_11 / rate_1, b = beta_21 / rate_1, c = beta_22 / rate_2, d = beta_12 / rate_2)
     return homo_probability, 1 - hetero_solutions[0]
-#print(homo_vs_hetero_probability(beta_11=0.6, beta_12=0.1, beta_21=0.2, beta_22=0.4, gamma_1=0.3, gamma_2=0.2))
 
 
 def vary_beta_11_beta_21(beta_11, beta_12, beta_21, beta_22, gamma_1, gamma_2):
-    """Function description"""
+    """Varies the values of beta_11 and beta_21 while maintaining the same naive values for the infection parameters"""
     accuracy = 100
     constant = beta_11 + beta_21
     beta_11_values = [i / accuracy for i in range(1, int(np.floor(constant * accuracy)))]
@@ -121,15 +121,9 @@ def vary_beta_11_beta_21(beta_11, beta_12, beta_21, beta_22, gamma_1, gamma_2):
     plt.legend()
     plt.show()
 
-#vary_beta_11_beta_21(beta_11=0.6, beta_12=0.1, beta_21=0.2, beta_22=0.4, gamma_1=0.3, gamma_2=0.2)
-#vary_beta_11_beta_21(beta_11=0.6, beta_12=0.1, beta_21=0.2, beta_22=0.05, gamma_1=0.3, gamma_2=0.2)
-#vary_beta_11_beta_21(beta_11=0.2, beta_12=0.2, beta_21=0.2, beta_22=0.1, gamma_1=0.3, gamma_2=0.15)
-#vary_beta_11_beta_21(beta_11=0.2, beta_12=0.2, beta_21=0.2, beta_22=0.2, gamma_1=1/2, gamma_2=1/8)
-#print(homo_from_hetero(beta_11=0.2, beta_12=0.3, beta_21=0.2, beta_22=0.1, gamma_1=1, gamma_2=1/9))
-
 
 def compute_dominant_eigenvalue(beta_11, beta_12, beta_21, beta_22, gamma_1, gamma_2):
-    """Function description"""
+    """Compute the dominant eigenvalue of the next generation matrix"""
     A = gamma_1 * gamma_2
     B = beta_11 * gamma_2 + beta_22 * gamma_1
     C = beta_11 * beta_22 - beta_12 * beta_21
@@ -138,7 +132,7 @@ def compute_dominant_eigenvalue(beta_11, beta_12, beta_21, beta_22, gamma_1, gam
 
 
 def dominant_eigenvalue_vary_beta_11_beta_21(beta_11, beta_12, beta_21, beta_22, gamma_1, gamma_2):
-    """Function description"""
+    """Varies beta_11 and beta_21 while maintaining the dominanat eigenvalue of the next generation matrix unchanged"""
     dominant_eigenvalue = compute_dominant_eigenvalue(beta_11, beta_12, beta_21, beta_22, gamma_1, gamma_2)
 
     varied_beta_11_21 = []
@@ -161,8 +155,7 @@ def dominant_eigenvalue_vary_beta_11_beta_21(beta_11, beta_12, beta_21, beta_22,
     plt.ylabel('Major outbreak probability', fontsize=15)
     plt.legend()
     plt.show()
-#dominant_eigenvalue_vary_beta_11_beta_21(beta_11=0.6, beta_12=0.1, beta_21=0.2, beta_22=0.1, gamma_1=0.3, gamma_2=0.15)
-dominant_eigenvalue_vary_beta_11_beta_21(beta_11=0.2, beta_12=0.2, beta_21=0.2, beta_22=0.2, gamma_1=1/2, gamma_2=1/8)
+
 
 def predicted_major_outbreak_calculator_from_pickle(file_name, directory='heterogeneous_SIR'):
     """Computes the probability of a major outbreak starting from 1 individual from the pickle file"""
@@ -219,58 +212,35 @@ def final_distibution_from_hetero_pickle(file_name, bins=100, directory='heterog
         plt.savefig(path, format=figure_format)
     plt.show()
 
-def draw_my_curves(a,b,c,d,xlim=(-1,2),ylim=(-1,2), save_figure=False, figure_format='eps', figure_name=' '):
-    x_range = np.arange(0.001, 2, 0.001)
-    y_range = [x * (-a / b) + (1 / b) + (1 / x) * (a + b - 1) / b for x in x_range]
-    plt.plot(x_range,y_range)
-    z_range = [x * (-c / d) + (1 / d) + (1 / x) * (c + d - 1) / d for x in x_range]
-    plt.plot(z_range, x_range)
-    plt.axis('scaled')
-    plt.xlim(xlim)
-    plt.ylim(ylim)
-    plt.axvline(0, color='black', alpha=0.5)
-    plt.axhline(0, color='black', alpha=0.5)
 
-    constant_range = [1 for i in range(100)]
-    values_range = [i/100 for i in range(100)]
-    plt.plot(constant_range, values_range, color='grey', linestyle='--')
-    plt.plot(values_range, constant_range, color='grey', linestyle='--')
-
-    if save_figure:
-        path = 'figures\\' + figure_name + '.' + figure_format
-        plt.savefig(path, format=figure_format)
+def vaccination_optimisation(N_1, N_2, beta_11, beta_12, beta_21, beta_22, gamma_1, gamma_2):
+    """Draws the function v(d1) over the prescribed interval"""
+    sign_term = beta_11 * gamma_2 + beta_12 * beta_21 - beta_11 * beta_22
+    if sign_term > 0:
+        lower_bound = max(0, gamma_1 * (gamma_2 - beta_22) / (beta_11 * gamma_2 + beta_12 * beta_21 - beta_11 * beta_22))
+        upper_bound = min(1, gamma_1 / beta_11)
+    else:
+        lower_bound = 0
+        upper_bound = min(1, gamma_1 / beta_11, gamma_1 * (gamma_2 - beta_22) / (beta_11 * gamma_2 + beta_12 * beta_21 - beta_11 * beta_22))
+    print(lower_bound, upper_bound)
+    d_range = [i/10000 for i in range(int(lower_bound * 10000), int(upper_bound * 10000))]
+    v_range = [int(N_1 * d_1 + N_2 * (gamma_1 * gamma_2 - d_1 * gamma_2 * beta_11) / (beta_22 * gamma_1 + d_1 * (beta_12 * beta_21 - beta_11 * beta_22))) for d_1 in d_range]
+    print(v_range)
+    plt.plot(d_range, v_range, color='green')
+    plt.xlabel('d1', fontsize=20)
+    plt.ylabel('v(d1)', fontsize=20)
     plt.show()
-
-
-#draw_my_curves(0.1,0.3,0.1,0.25, save_figure=False, figure_name='yes_intersection_in_square')
+    
+"""Example uses for the functions above:"""
+#vaccination_optimisation(N_1=1000, N_2=1000, beta_11=0.7, beta_12=0.1, beta_21=0.2, beta_22=0.3, gamma_1=0.25, gamma_2=0.2)
 #pickle_heterogeneity_data(file_name='1000_runs', runs=1000, N_1=400, N_2=1600, I_1=1, I_2=0, beta_11=0.7, beta_12=0.1, beta_21=0.2, beta_22=0.3, gamma_1=0.25, gamma_2=0.2)
-#draw_infection_from_pickle_hetero(file_name='1000_runs', run=1, separate_plots=True)
-
-#pickle_heterogeneity_data(file_name='depletion', runs=100, N_1=250, N_2=750, I_1=1, I_2=0, beta_11=1, beta_12=0.25, beta_21=0.35, beta_22=0.1, gamma_1=0.2, gamma_2=0.2)
 #print(compare_predicted_and_empirical('children_adults'))
 #draw_infection_from_pickle_hetero('depletion',separate_plots=True, run=2, save_figure=False, figure_name='just_children', figure_format='png', figure_title='Children', figure_title_2='Adults')
-
-#pickle_heterogeneity_data(file_name='mosquitos', runs=10, N_1=1000, N_2=10000, I_1=0, I_2=1000, beta_11=0, beta_12=0.005, beta_21=0.05, beta_22=0, gamma_1=0.2, gamma_2=0.01)
-#draw_infection_from_pickle_hetero('mosquitos',separate_plots=True, run=0, save_figure=False, figure_name='just_children', figure_format='png', figure_title='Humans', figure_title_2='Mosquitos')
-
-#pickle_heterogeneity_data(file_name='super-spreaders', runs=1000, N_1=10, N_2=990, I_1=0, I_2=1, beta_11=1, beta_12=0.35, beta_21=0.3, beta_22=0.08, gamma_1=0.15, gamma_2=0.1)
-#draw_infection_from_pickle(file_name='super-spreaders', run=10, separate_plots=True)
-#a,b,c,d=0.2,0.59,0.2,0.59
 #print(efficient_solver(a,b,c,d))
-#draw_my_curves(a,b,c,d,save_figure=True, figure_format='png', figure_name='presentation_3')
-
-
-#pickle_heterogeneity_data(file_name='naive_vs_hetero_2', runs=1000, N_1=5000, N_2=5000, I_1=0, I_2=1, beta_11=0.19, beta_12=0.07, beta_21=0.035, beta_22=0.1, gamma_1=0.2, gamma_2=0.2)
 #print(compare_predicted_and_empirical('naive_vs_hetero_2'))
-
-#test_vary_beta_12(0.6, 0.2, 0.4, 0.4)
-
-#pickle_heterogeneity_data(file_name='10000_runs_trimodal', runs=10000, N_1=1000, N_2=1000, I_1=1, I_2=0, beta_11=0.4, beta_12=0.0002, beta_21=0.0002, beta_22=0.4, gamma_1=0.2, gamma_2=0.2)
-#print(compare_predicted_and_empirical('10000_runs_trimodal'))
 #final_distibution_from_hetero_pickle('10000_runs_trimodal')
 #final_distibution_from_hetero_pickle('1000_runs')
-
 #unpickle_data('10000_runs', directory='heterogeneous_SIR', only_initial=True, print_initial=True)
-
-#draw_my_curves(a=0.15, b=0.6, c=0.15, d=0.5,)
-#draw_my_curves(a=0.15, b=0.3, c=0.15, d=0.2)
+#print(compare_predicted_and_empirical('10000_6'))
+#vary_beta_11_beta_21(beta_11=0.6, beta_12=0.1, beta_21=0.2, beta_22=0.4, gamma_1=0.3, gamma_2=0.2)
+#dominant_eigenvalue_vary_beta_11_beta_21(beta_11=0.6, beta_12=0.1, beta_21=0.2, beta_22=0.1, gamma_1=0.3, gamma_2=0.15)
